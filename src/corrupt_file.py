681832f5df258788d62ab963339cf575cf0f8d84
Antolvge <lavanto@live.fr>
2025-06-20 12:24:32 +0200

    Add tvl1 and optimizations

--- src/corrupt_file.py
+++ src/corrupt_file.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 from random import randrange
 import matplotlib.pyplot as plt
 import os
@@ -20,7 +21,7 @@ def create_mask_micro(image, coverage, k, s):
     
     Return:
     -------
-    corrupt_img (array): Corrupted image
+    corrupt_img (array): Corrupted image composed of micro-paths measurements of length k with a minimal distance between them of s pixels and total coverage of coverage.
     
     To be noted: the k and s parameters cannot be any value and depend on the AFM.
     In our case, k must be greater than 15 pixels and s greater than 6 pixels (4 + the 2 pixels at the ends of the micro-paths).
@@ -35,7 +36,7 @@ def create_mask_micro(image, coverage, k, s):
     
     l_row = len(image[0])
     n_pix = len(image) * len(image[0]) # In the case of rectangle images (not for AFM but SEM?)
-    pix_cov = n_pix * coverage # Number of pixel with non-zero measurement
+    pix_cov = n_pix * coverage # Number of pixels with non-zero measurement
     n_paths = int(pix_cov // k) # Number of micro-paths
 
     corrupt_img = np.zeros_like(image)
@@ -43,7 +44,7 @@ def create_mask_micro(image, coverage, k, s):
         row_p = randrange(len(image)) # Select a starting point for the micro-path
         col_p = randrange(l_row)
         t = 0
-        # Verify that it isn't at the en of a row, that it doesn't overlap with another path and that there is enough space between 2 paths
+        # Verify that it isn't at the end of a row, that it doesn't overlap with another path and that there is enough space between 2 paths
         while   (col_p + k > l_row) or \
                 (corrupt_img[row_p, col_p] != 0) or \
                 (corrupt_img[row_p, col_p+k-1] != 0) or \
@@ -52,7 +53,7 @@ def create_mask_micro(image, coverage, k, s):
             row_p = randrange(len(image))
             col_p = randrange(l_row)
             t += 1
-            if t > 50:
+            if t > 1000:
                 raise MethodError("The coverage is probably too high and the micropaths are not the most appropriate technique")
         for j in range(k):
             corrupt_img[row_p][col_p+j] = image[row_p][col_p+j]
@@ -60,17 +61,46 @@ def create_mask_micro(image, coverage, k, s):
 
 
 # Simulate a measurement where only 1 every 2 pixels is measured in every direction. In that case, only a quarter of the image is measured.
-def create_mask_quarter(image):
+def create_mask_quarter(image, begin):
     if (not begin.is_integer()) or begin < 1 or begin > 4:
         raise ValueError("'begin' must be equal to 1, 2, 3 or 4")
     i0 = (begin-1)%2
     j0 = (begin-1)//2
     n = len(image)
-    corrupt_img = np.zeros((n,n))
+    corrupt_img = np.zeros_like(image)
     for i in range(i0,n,2):
         for j in range(j0,n,2):
             corrupt_img[i,j] = image[i,j] # Only take 1 every 2 pixels 
     return corrupt_img
+
+
+def create_mask_micro_unif(image, k, period, begin):
+    """Create an image with micro-paths of length k seperated by k pixels. The coverage is 50 %"""
+
+    if not math.log2(k).is_integer():
+        raise ValueError("k must be a power of 2")
+    if not isinstance(k//period, int):
+        raise ValueError("period must be an integer that divides k")
+    if not begin.is_integer() or begin <= 0 or begin > k:
+        raise ValueError("begin must be a integer between 1 and period")
+    n = len(image)
+    corrupt_img = np.zeros_like(image)
+    # For each line, the pattern is the same: k pixels measured followed by k pixels not measured and so on.
+    # On the next line, the pattern is horizontally translated by 2*k//period pixels.
+    # When the path reaches the end of the line, it stops with a length lesser than k
+    for i in range(0, n, period):
+        for j in range(0, n, 2*k//period):
+            l = (j*period//(2*k) + begin - 1) % period
+            corrupt_img[i+l ,j:j+k] = image[i+l, j:j+k]
+    # Add the partial paths at the beginning of each line in the case period > 2
+    if period > 2:
+        for i in range(n):
+            # End of the partial path at the beginning of each line. We add period//2 because, vertically, we start by no path
+            end = ((i+(begin-1)+period//2)%period) * 2*k//period
+            corrupt_img[i,max(0,end-k):end] = image[i,max(0,end-k):end]
+    return corrupt_img
+
+
 
 
 
@@ -78,7 +108,7 @@ if __name__ == "__main__":
 
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     DATA_DIR = os.path.join(BASE_DIR, "data")
-    file = "HA 2uM DOPC NTA 10 0.5mM_Sln_201202_153921.txt"
+    file = "1_TMV_0.1_Au_TSGs_RH10__amp 2V_150701_114145.txt"
     im_file = os.path.join(DATA_DIR, file)
 
     with open(im_file, 'r', encoding='latin1') as f:
@@ -92,20 +122,26 @@ if __name__ == "__main__":
 
 
     # Type of corrupt image. For now u-paths or quarter
-    c_type = 'u-paths' 
+    c_type = 'unif' 
 
     if c_type == 'quarter':
         begin = 1 # Possible values: 1, 2, 3 or 4 to select which first point to select in the top left 2x2 square of the image
         extension = '_quarter_' + str(begin)
-        corrupt_img = create_mask_quarter(image)
+        corrupt_img = create_mask_quarter(image, begin)
     elif c_type == 'u-paths':
         coverage = 0.5 # Fraction of the surface measured
-        k = 10 # Length of the micro-paths
-        s = 0 # Space between 2 micro-paths
-        extension = str(int(coverage*100)) + "_k" + str(k) + "_1"
+        k = 15 # Length of the micro-paths
+        s = 6 # Space between 2 micro-paths
+        extension = str(int(coverage*100)) + "_k" + str(k) + "_s" + str(s) + "_5"
         corrupt_img = create_mask_micro(image, coverage, k, s)
+    elif c_type == 'unif':
+        k = 16 # Length of the micro-paths
+        period = 8 # Number of lines of the pattern. Must be a divider of k
+        begin = 1 # Integer between 1 and 'period'. Line of the pattern from which to start.
+        extension = f"_unif_k{k}_per{period}_{begin}"
+        corrupt_img = create_mask_micro_unif(image, k, period, begin)
     else:
-        raise ValueError(f"the algorithm {c_type} is not recognized. Try 'quarter' or 'u-paths' instead.")
+        raise ValueError(f"the algorithm {c_type} is not recognized. Try 'quarter', 'unif' or 'u-paths' instead.")
 
 
     # Comparison plot between original and corrupted images
@@ -120,4 +156,4 @@ if __name__ == "__main__":
     # File saving
     output_file = im_file[:-4] + "_corrupt" + extension + ".txt"
     np.savetxt(output_file, corrupt_img, fmt='%.4f', delimiter="\t")
-    print("File saved", output_file)
+    print(f"\nFile saved: {output_file}\n")
